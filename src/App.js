ddbdc1791d8049e1837ca9622561d908dda490d4
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-28 21:21:57 +0530

    color-generator

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15a6c").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15a6c"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((item, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...item}
+              index={index}
+              hexColor={item.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/SingleColor.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ weight, rgb, hexColor, index }) => {
+  const hexValue = `#${hexColor}`;
+  const [alert, setAlert] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: hexValue }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to Clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
